6797543fc19ca97f29d750e07bc499a678e170e7
Carlos del-Castillo-Negrete <cdelcastillo21@gmail.com>
2022-12-04 19:41:25 -0600

    :fire: Moving old tacc container directory
    Remove:
     + TACC container directories already in containers folder.

--- tacc-ubuntu18-mvapich2.3-ib/vim_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RUN micromamba activate dev && \
-    micromamba install -y vim fzf -c conda-forge && \
-    cp /data/vimrc ~/.vimrc && \
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ && \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
-    vim +PlugInstall +qall && \
