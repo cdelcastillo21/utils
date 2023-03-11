bb33bf9b9c2b11422bc74995e1cdd2beadc6bc11
Carlos del-Castillo-Negrete <cdelcastillo21@gmail.com>
2023-03-11 15:55:40 -0600

    :sparkles: More DesignSafe configuration

--- DesignSafe/fish_greeting.fish
+++ DesignSafe/fish_greeting.fish
@@ -10,69 +10,20 @@ function fish_greeting
         end
 
         if type -q cowsay
-            cowsay -w 80 -r (fortune) | lolcat 
+            cowsay -w 80 -r (fortune ~/.config/fish/fortunes.txt) | lolcat 
         end
 
         if type -q neofetch
             neofetch
         end
 
-        if type -q duf
-            duf -theme dark --width 80 --only-mp /,/System/Volumes/Data
-        end
         if type -q dust
             dust -z 500MB -i -n 10 -d 5 -w 80 $WORK 2> /dev/null
         end
     end
 
-
-    if type -q tmux
-        if status is-interactive
-            default_greeting
-	end
-        if status is-interactive
-    	and not set -q TMUX
-    	    set tmux_res (tmux ls 2> /dev/null)
-    	    set first_session (string split : $tmux_res | head -n 1)
-	    switch $first_session
-                case ''
-    	            echo "No server running. Name session (default:0):" | lolcat 
-    	    	    read -l -P '' value
-		    switch $value
-		        case ''
-		            tmux
-		        case '*'
-	                    tmux new-session -s $value
-		    end
-		case '*'
-	 	    set test (string join '\n' "======== Active TMUX Sessions ========" \
-		    		     $tmux_res \
-				     "Choose an active session or start a new one:" \
-				     "[<name-of-session>/Y(defaults to first session)/N(new session-DEFAULT)]"
-				     "")
-		    printf $test | lolcat
-    	            read -l -P '' value
-    	            switch $value
-                        case ''
-	                    echo "value '$value'"
-	                    tmux
-    	                case 'N'
-	                    tmux
-	                    echo "value '$value'"
-    	                case 'Y'
-    	                    set first_session (string split : $tmux_res | head -n 1)
-	                    echo "Switching to first session $first_session"
-    	                    tmux a -t $first_session
-    	                case '*'
-	                    echo "Attaching or creating session $value"
-    	                    tmux new-session -A -s $value
-	            end
-	    end
-    	end
-    else
-        if status is-interactive
-            default_greeting
-        end
+    if status is-interactive
+	default_greeting
     end
 
 end
